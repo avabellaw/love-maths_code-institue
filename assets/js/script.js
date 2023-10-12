4d9e6e36aa6e6a48b1b160c0329a1a0eaab336b7
avabellaw <70448139+avabellaw@users.noreply.github.com>
2023-10-12 17:09:39 +0100

    Add code to check addition answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
     for(let button of buttons){
         button.addEventListener('click', function(){
             if(this.getAttribute("data-type") === "submit"){
-                alert('submit');
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -35,16 +35,47 @@ function runGame(gameType){
  }
 
  function setOperands(num1, num2){
-    document.getElementById("operand1").innerHTML = num1;
+    document.getElementById("operand1").textContent = num1;
     document.getElementById("operand2").textContent = num2;
  }
 
+/**
+ * Checks answer against calculateCorrectAnswer()
+ */
 function checkAnswer(){
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let correctAnswer = calculateCorrectAnswer();
 
+    if(userAnswer === correctAnswer[0]){ alert("correct")}
+    else {
+        alert(`nope... correct answer is ${correctAnswer[0]}`);
+    }
+
+    console.log(correctAnswer[1]);
+    runGame(correctAnswer[1]);
 }
 
+/**
+ * Gets the operands and the operator
+ * returns correct answer
+ */
 function calculateCorrectAnswer(){
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
 
+    if(operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else if(operator === "-"){
+        return [operand1 - operand2, "subtraction"];
+    } else if(operator === "*"){
+        return [operand1 * operand2, "multiplication"];
+    } else if(operator === "/"){
+        return [operand1 / operand2, "Division"];
+    } else {
+        alert(`${operator} - Not recognised`);
+        throw `${operator} - Not recognised`;
+    }
 }
 
 function incrementScore(){
